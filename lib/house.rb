ef14ceafaee3a368c957876bb39d45e96de59e3a
Myles Moylan <moylanm@oregonstate.edu>
2022-04-22 08:33:54 -0700

    add cases for lines 2 and 3

--- lib/house.rb
+++ lib/house.rb
@@ -3,6 +3,10 @@ class House
     case number
     when 1
       "This is the house that Jack built.\n"
+    when 2
+      "This is the malt that lay in the house that Jack built.\n"
+    when 3
+      "This is the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
 end
