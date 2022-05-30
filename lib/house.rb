799e8a6a78e96023a2c88fd75ae598c3f77d50b4
Myles Moylan <moylanm@oregonstate.edu>
2022-05-30 11:20:08 -0700

    delete case statement and use line_helper method

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,7 @@
 class House
 
   @@phrases = [
-    "the house that Jack built.",
+    "the house that Jack built",
     "the malt that lay in",
     "the rat that ate",
     "the cat that killed",
@@ -20,37 +20,16 @@ class House
   end
 
   def line(number)
-    case number
-    when 1
-      "This is the house that Jack built.\n"
-    when 2
-      "This is the malt that lay in the house that Jack built.\n"
-    when 3
-      "This is the rat that ate the malt that lay in the house that Jack built.\n"
-    when 4
-      "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 5
-      "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 6
-      "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 7
-      "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 8
-      "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 9
-      "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 10
-      "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 11
-      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    when 12
-      "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
-    end
+    "This is #{line_helper(number)}.\n"
   end
 
   private
 
   def line_helper(number)
-
+    if number == 1
+      @@phrases[number - 1]
+    else
+      "#{@@phrases[number - 1]} #{line_helper(number - 1)}"
+    end
   end
 end
